a9d86ff4c97ded29ba0327c45a2f36c5083e1460
HEBERT MORAES SERRA <hebertmoraes2019@gmail.com>
2023-01-27 20:05:33 -0300

    melhoria na camera na sala
    Agora a camera das salas não é capaz de passar pela parede e gravar o mundo fora da sala.

--- src/Assets/Scripts/Sala/MovCameraSala.cs
+++ src/Assets/Scripts/Sala/MovCameraSala.cs
@@ -4,46 +4,35 @@ using UnityEngine;
 
 public class MovCameraSala : MonoBehaviour
 {
-    public int indiceParedeEsquerda;
-    public int indiceParedeDireita;
-    private GameObject sala;
+    public float posXLimiteMin;
+    public float posXLimiteMax;
     private float velocidadeMovPlayer;
-    private float posXLimiteMin;
-    private float posXLimiteMax;
-    private float xMove;
+
     // Start is called before the first frame update
     void Start()
     {
         velocidadeMovPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<MovBasicaSala>().velocidadeMov;
-
-        sala = GameObject.FindGameObjectWithTag("Sala");
-        posXLimiteMin = sala.transform.GetChild(indiceParedeEsquerda).position.x;
-        posXLimiteMax = sala.transform.GetChild(indiceParedeDireita).position.x;
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        xMove = Input.GetAxis("Horizontal");
-
-        if (transform.position.x < posXLimiteMin) {
+    {
+        if (transform.position.x + (Input.GetAxis("Horizontal") * velocidadeMovPlayer * Time.deltaTime) >= 
+            posXLimiteMin && Input.GetAxis("Horizontal") < 0)
+        {
             transform.position = new Vector3(
-                posXLimiteMin, 
-                transform.position.y, 
-                transform.position.z);
-        }
-        if (transform.position.x > posXLimiteMax) {
-            transform.position = new Vector3(
-                posXLimiteMax, 
-                transform.position.y, 
-                transform.position.z);
+                    transform.position.x + (Input.GetAxis("Horizontal") * velocidadeMovPlayer * Time.deltaTime),
+                    transform.position.y,
+                    transform.position.z);
         }
 
-        if (transform.position.x >= posXLimiteMin && transform.position.x <= posXLimiteMax) {
+        if (transform.position.x + (Input.GetAxis("Horizontal") * velocidadeMovPlayer * Time.deltaTime) <= 
+            posXLimiteMax && Input.GetAxis("Horizontal") > 0)
+        {
             transform.position = new Vector3(
-                transform.position.x + (xMove * velocidadeMovPlayer * Time.deltaTime), 
-                transform.position.y, 
-                transform.position.z);
+                    transform.position.x + (Input.GetAxis("Horizontal") * velocidadeMovPlayer * Time.deltaTime),
+                    transform.position.y,
+                    transform.position.z);
         }
     }
 }
