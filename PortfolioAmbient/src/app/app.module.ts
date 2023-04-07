966aa5d1dcd2220ac3011a0862c7c782dc49c489
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2023-04-07 00:40:35 -0300

    nova tentativa
    seguido totalmente o tutorial em https://joemoceri.medium.com/deploying-unity-2020-3-2-webgl-project-using-angular-11-312cb33e6fbe

--- PortfolioAmbient/src/app/app.module.ts
+++ PortfolioAmbient/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameUnityComponent } from './game-unity/game-unity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameUnityComponent,
   ],
   imports: [
     BrowserModule,
